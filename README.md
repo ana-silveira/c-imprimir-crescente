7225b3fb60b1703bfe5ecf2be85b40af88d7762f
Ana Carolina <ana.fbsilveira@gmail.com>
2022-09-06 16:57:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# exercicio41
+ Faça um programa que imprima os números de 1 a 100.
---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+41. Faça um programa que imprima os números de 1 a 100.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    system("cls");
+
+    for ( int i = 1; i <=100; i++){
+        printf("%d\n", i);
+
+    }
+
+    system("pause");
+
+}
